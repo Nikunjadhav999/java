ba6327e68eecd06e30c9434b4a2f8fe34097f67b
Nikunjadhav999 <nikunjjadhav43@gmail.com>
2025-02-01 17:18:46 +0530

    Commiting to dev day1

---
+++ wipro_java_project/src/com/java/wipro/MyClass.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.java.wipro;
+
+/**
+ * @author Nikunj
+ */
+public class MyClass {
+
+	/**
+	 * 
+	 */
+
+		// TODO Auto-generated constructor stub
+		 int num = 5;
+		 
+		    public void changeValue(int num) {
+
+		        this.num = num;
+
+		    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        MyClass obj = new MyClass();
+
+        obj.changeValue(10);
+
+        System.out.println(obj.num);
+
+    }
+
+}
+
+ 
