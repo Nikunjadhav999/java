9e096b91228f1b8949dfa8c65eb453c38e1b1674
Nikunjadhav999 <nikunjjadhav43@gmail.com>
2025-02-01 17:06:39 +0530

    commiting to dev

--- wipro_java_project/src/com/java/wipro/Flower.java
+++ wipro_java_project/src/com/java/wipro/Flower.java
@@ -30,7 +30,7 @@ public class Flower {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Flower flowerobj1=new Flower("Rose","Red.");
+		Flower flowerobj1=new Flower("Rose","Red..");
 		Flower flowerobj2=new Flower("Lotus","Pink.");
 		flowerobj1.display();
 		flowerobj2.display();
