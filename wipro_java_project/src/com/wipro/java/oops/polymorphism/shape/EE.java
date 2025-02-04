48fba92192a1a0e3a9a8bc22a00e05c08c919aa7
Nikunjadhav999 <nikunjjadhav43@gmail.com>
2025-02-04 12:00:09 +0530

    commit to dev day3

--- wipro_java_project/src/com/wipro/java/oops/polymorphism/shape/EE.java
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/shape/EE.java
@@ -1,7 +1,11 @@
 package com.wipro.java.oops.polymorphism.shape;
-
+/*
+ * @authoe Nikunj
+ */
+//extending the superclass
 public class EE extends College 
 {
+	//show all the details of department
 	 public void showDetails()
 	  {
 		System.out.println("Details of the Department in college: department:  "+ getDepartment()+"  Hod: "+ getHod()+" No Of Students:"+getStudent());
@@ -9,9 +13,9 @@ public class EE extends College
 	   public static void main(String[] args) 
 	   {   
 	    	College c1=new EE();//create a object of EE constructor using superclass
-	    	c1.setDepartment("EE");//
-	    	c1.setHod("Prof Dharmik");
-	    	c1.setStudent(130);
+	    	c1.setDepartment("EE");// set department 
+	    	c1.setHod("Prof Dharmik");//set hod name
+	    	c1.setStudent(130);//set no of students
 	    	c1.showDetails();//print all the details
 	    	
 		}
--- wipro_java_project/src/com/wipro/java/oops/polymorphism/shape/IT.java
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/shape/IT.java
@@ -1,11 +1,12 @@
 package com.wipro.java.oops.polymorphism.shape;
+/*
+ * author Nikunj
+ */
 
-
-
+//extending the base class
 public class IT extends College{
 	
-
-	
+	//show the details of department
 	 public void showDetails()
 	  {
 		System.out.println("Details of the Student: department: "+ getDepartment()+"  Hod "+ getHod()+"  No Of Students: "+getStudent());
@@ -13,9 +14,9 @@ public class IT extends College{
 	   public static void main(String[] args) 
 	   {   
 	    	College c1=new IT();//create a object of IT constructor using superclass
-	    	c1.setDepartment("IT");//
-	    	c1.setHod("Prof Sushil");
-	    	c1.setStudent(100);
+	    	c1.setDepartment("IT");//set department 
+	    	c1.setHod("Prof Sushil");//set Hod name
+	    	c1.setStudent(100);//set No Of students
 	    	c1.showDetails();//print all the details
 	    	
 		}
